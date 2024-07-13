68457420564a538755db9d09893b6479fcad82d5
Sushiii2003 <sushantkumar0704@gmail.com>
2024-07-14 02:03:21 +0530

    first commit

---
+++ index1.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+app.get('/twitter', (req,res)=>{
+    res.send('sushantdotcom')
+})
+app.get('/login',(req,res)=>{
+    res.send('<h1>please login</h1>')
+})
+app.get('/youtube',(req,res)=>{
+    res.send('<h1>chai aur code</h1>')
+})
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
